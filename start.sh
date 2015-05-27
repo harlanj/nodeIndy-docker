862075c9719be5faa0406113c33ab45f6b475c56
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2015-05-27 16:22:37 -0400

    minimize dockerfile

--- start.sh
+++ start.sh
@@ -11,12 +11,12 @@ docker pull tutum/haproxy
 
 # Create 4 containers with node app
 echo "Creating and starting 4 node apps in docker containers"
-docker run -dt -v /var/logs:logs --name socketio1 socketio
-docker run -dt -v /var/logs:logs --name socketio2 socketio
-docker run -dt -v /var/logs:logs --name socketio3 socketio
-docker run -dt -v /var/logs:logs --name socketio4 socketio
+docker run -dt -v /var/logs:/logs --name socketio1 socketio
+docker run -dt -v /var/logs:/logs --name socketio2 socketio
+docker run -dt -v /var/logs:/logs --name socketio3 socketio
+docker run -dt -v /var/logs:/logs --name socketio4 socketio
 
 # Create haproxy container and link all node apps in docker containers
 # The -e flag overrides environment variables. It will override environment variables set by the Dockerfile also
 echo "Running haproxy docker container and linking node app containers for load balancing"
-docker run -de -v /var/logs:logs -p 80:80 --link socketio1:socketio1 --link socketio2:socketio2 --link socketio3:socketio3 --link socketio4:socketio4 BACKEND_PORT=8080 tutum/haproxy
+docker run -dt -v /var/logs:logs -p 80:80 --link socketio1:socketio1 --link socketio2:socketio2 --link socketio3:socketio3 --link socketio4:socketio4 -e BACKEND_PORT=8080 tutum/haproxy
